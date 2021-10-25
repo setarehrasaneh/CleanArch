efdaf467f7d72e70c25947c3e1d3bf80ced717fb
SETAREH <setare_r2000@yahoo.com>
2021-10-25 19:51:04 +0330

    Implement IOC

---
+++ CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServises(IServiceCollection servise)
+        {
+            servise.AddScoped<ICourseService, CourseService>();
+            servise.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
